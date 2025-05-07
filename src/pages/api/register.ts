585b3d7580b727a65c84cc91af2ef25a67f22d76
RamonUlises <jorgitomilina@gmail.com>
2025-05-06 22:10:43 -0600

    Agrega la ruta API para el registro de usuarios con validación de datos y manejo de errores.

---
+++ src/pages/api/register.ts
@@ -0,0 +1,27 @@
+export const prerender = false;
+
+import type { APIRoute } from "astro";
+import sql from "@lib/database";
+
+export const POST: APIRoute = async ({ request }) => {
+  console.log(request.headers.get("Content-Type"))
+  if(request.headers.get("Content-Type") !== "application/json") {
+    return new Response(JSON.stringify({ message: "El contenido del body debe ser JSON" }), { status: 400 });
+  }
+
+  const { nombre, correo, contrasenia, fecha_nacimiento, id_sexo } = await request.json();
+
+  if (!nombre || !correo || !contrasenia || !fecha_nacimiento || !id_sexo) {
+    return new Response(JSON.stringify({ message: "Las credenciales son obligatorias" }), { status: 400 });
+  }
+
+  sql.then(async (con) => {
+    await con.query`INSERT INTO usuarios(nombre, correo, contrasenia, fecha_nacimiento, id_sexo, id_tipo_usuario) VALUES(${nombre}, ${correo}, ${contrasenia}, ${fecha_nacimiento}, ${id_sexo}, 2)`;
+  })
+  .catch((err) => {
+    console.log(err);
+    return new Response(JSON.stringify({ message: "Error al registrar el usuario" }), { status: 500 });
+  });
+
+  return new Response(JSON.stringify({ message: "Credenciales válidas" }), { status: 200 });
+};
