1a10ceb1517449e0a5f6b77c0997c1a898634a9c
RamonUlises <jorgitomilina@gmail.com>
2025-05-07 10:52:17 -0600

    Refactor registro de usuario: mejora la validación de entrada y utiliza encriptación de contraseñas

--- src/pages/api/register.ts
+++ src/pages/api/register.ts
@@ -2,28 +2,53 @@ export const prerender = false;
 
 import type { APIRoute } from "astro";
 import sql from "@lib/database";
+import { encrypt } from "@lib/encripts";
 
 export const POST: APIRoute = async ({ request }) => {
-  if(request.headers.get("Content-Type") !== "application/json") {
-    return new Response(JSON.stringify({ message: "El contenido del body debe ser JSON" }), { status: 400 });
+  if (request.headers.get("Content-Type") !== "application/json") {
+    return new Response(
+      JSON.stringify({ message: "El contenido del body debe ser JSON" }),
+      { status: 400 },
+    );
   }
 
-  const { nombre, correo, contrasenia, fecha_nacimiento, id_sexo } = await request.json();
+  const { nombre, correo, contrasenia, fecha_nacimiento, id_sexo } =
+    await request.json();
 
   if (!nombre || !correo || !contrasenia || !fecha_nacimiento || !id_sexo) {
-    return new Response(JSON.stringify({ message: "Las credenciales son obligatorias" }), { status: 400 });
+    return new Response(
+      JSON.stringify({ message: "Las credenciales son obligatorias" }),
+      { status: 400 },
+    );
   }
 
-  sql.then(async (con) => {
-    const response = await con.query`INSERT INTO usuarios(nombre, correo, contrasenia, fecha_nacimiento, id_sexo, id_tipo_usuario) VALUES(${nombre}, ${correo}, ${contrasenia}, ${fecha_nacimiento}, ${id_sexo}, 2)`;
+  try {
+    const contra = await encrypt(contrasenia);
+    await sql
+      .request()
+      .input("nombre", nombre)
+      .input("correo", correo)
+      .input("contrasenia", contra)
+      .input("fecha_nacimiento", fecha_nacimiento)
+      .input("id_sexo", id_sexo)
+      .execute("sp_crear_usuario");
 
-    if(response.rowsAffected.length < 1) {
-      return new Response(JSON.stringify({ message: "Error al registrar el usuario" }), { status: 500 });
+    return new Response(
+      JSON.stringify({ message: "Usuario registrado correctamente" }),
+      { status: 200 },
+    );
+  } catch (err: any) {
+    console.log(err);
+    if (err.number === 50001) {
+      return new Response(
+        JSON.stringify({ message: "El correo ya está registrado" }),
+        { status: 400 },
+      );
     }
-  })
-  .catch(() => {
-    return new Response(JSON.stringify({ message: "Error al registrar el usuario" }), { status: 500 });
-  });
 
-  return new Response(JSON.stringify({ message: "Credenciales válidas" }), { status: 200 });
+    return new Response(
+      JSON.stringify({ message: "Error al registrar el usuario" }),
+      { status: 500 },
+    );
+  }
 };
