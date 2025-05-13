aa2f0335594f033635a9af6828895a7d5b9abf46
RamonUlises <jorgitomilina@gmail.com>
2025-05-13 08:22:42 -0600

    agrega la clave de API de Resend a las variables de entorno

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,6 +3,7 @@ interface ImportMetaEnv {
   readonly USER_DB: string;
   readonly PASSWORD_DB: string;
   readonly SERVER_DB: string;
+  readonly RESEND_API_KEY: string;
 }
 
 interface ImportMeta {
