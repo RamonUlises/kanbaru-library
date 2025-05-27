d6369e3758bbaf7eabb06b1c78c95c15188debe3
RamonUlises <jorgitomilina@gmail.com>
2025-05-27 17:15:38 -0600

    Agregar manejo de tema basado en preferencias del sistema y eliminar función checkTheme

--- src/utils/temaPage.ts
+++ src/utils/temaPage.ts
@@ -18,23 +18,3 @@ export function cambiarTema(tema: string){
     document.body.classList.remove('dark');
   }
 }
-
-export function checkTheme(){
-  const tema = localStorage.getItem('tema');
-
-  if(!tema || tema === 'sistema'){
-    localStorage.setItem('tema', 'sistema');
-
-    if(window.matchMedia('(prefers-color-scheme: dark)').matches){
-      document.body.classList.add('dark');
-    } else {
-      document.body.classList.remove('dark');
-    }
-  }
-
-  if(tema === 'oscuro'){
-    document.body.classList.add('dark');
-  } else if(tema === 'claro'){
-    document.body.classList.remove('dark');
-  }
-}
