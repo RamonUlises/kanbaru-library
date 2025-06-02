4d72a85b013ef8fa7ee3f2888944d0157d45eeca
RamonUlises <jorgitomilina@gmail.com>
2025-06-02 17:02:42 -0600

    card

---
+++ src/types/libros.ts
@@ -0,0 +1,11 @@
+export interface LibrosTypes {
+  id_libros: number,
+  titulo: string,
+  autor: string,
+  lenguaje: string,
+  editorial: string,
+  ejemplares: string,
+  descargable: boolean,
+  codigo_libro: string,
+  id_categoria: number
+}
